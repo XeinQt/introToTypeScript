3136cce1dae3e193eac16db26058a3565f05bb66
My Name <myemail@example.com>
2025-10-22 15:17:22 +0800

    fundamentals of typescript

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-var age = 20;
+// let age: number = 20;
+// console.log(`Age is: ${age}`);
+// let greet: string = "Hello, World!";
+// console.log(greet);
+// let isActive: boolean = true;
+// if (isActive) {
+//   console.log("The system is active.");
+// }
+// let username: string = "RicoAlentijo";
+// let password: string = "securePssword123";
+// if (username === "RicoAlentijo" && password === "securePassword123") {
+//   console.log("Access granted.");
+// } else {
+//   console.log("Access denied");
+// }
+var username = "RicoAletijo";
+var password = "securePassword123";
+function login(user, pass) {
+    if (user === "RicoAlentijo" && pass === "securePassword123") {
+        console.log("access granted");
+    }
+    else {
+        console.log("access denied");
+    }
+}
+login(username, password);
--- index.ts
+++ index.ts
@@ -1,1 +1,34 @@
-let age: number = 20;
+// let age: number = 20;
+
+// console.log(`Age is: ${age}`);
+
+// let greet: string = "Hello, World!";
+// console.log(greet);
+
+// let isActive: boolean = true;
+
+// if (isActive) {
+//   console.log("The system is active.");
+// }
+
+// let username: string = "RicoAlentijo";
+// let password: string = "securePssword123";
+
+// if (username === "RicoAlentijo" && password === "securePassword123") {
+//   console.log("Access granted.");
+// } else {
+//   console.log("Access denied");
+// }
+
+// let username: string = "RicoAletijo";
+// let password: string = "securePassword123";
+
+// function login(user: string, pass: string) {
+//   if (user === "RicoAlentijo" && pass === "securePassword123") {
+//     console.log("access granted");
+//   } else {
+//     console.log("access denied");
+//   }
+// }
+
+// login(username, password);
